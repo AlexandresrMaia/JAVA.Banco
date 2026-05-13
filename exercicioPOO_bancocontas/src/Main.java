493e9674701ff05f5c8ec37d20ca11c52dac446e
AlexandresrMaia <alexandresrmaia@gmail.com>
2026-05-12 22:58:44 -0300

    Update Main.java

--- exercicioPOO_bancocontas/src/Main.java
+++ exercicioPOO_bancocontas/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.println("BEM VINDO AO SISTEMA DE PAGAMENTOS OKANE");
+        System.out.println("BEM VINDO AO SISTEMA DE PAGAMENTOS");
 
         System.out.print("Digite o saldo inicial da Conta Corrente: ");
         double saldoCC = leitor.nextDouble();
